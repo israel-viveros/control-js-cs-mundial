d596c11914fb6a2308c8800ed6a66975995faa59
Israel Viveros <jose.viveros@esmas.net>
2013-12-05 19:08:58 -0600

    Fix QA fase 1

--- js/timeline.js
+++ js/timeline.js
@@ -1,6 +1,7 @@
+
 /*
   Developer: Israel Viveros;
-  Version: 1.0;
+  Version: 1.1;
 */
 // Jquery.jsonp 2.4.0
 (function(e){function t(){}function n(e){C=[e]}function r(e,t,n){return e&&e.apply&&e.apply(t.context||t,n)}function i(e){return/\?/.test(e)?"&":"?"}function O(c){function Y(e){z++||(W(),j&&(T[I]={s:[e]}),D&&(e=D.apply(c,[e])),r(O,c,[e,b,c]),r(_,c,[c,b]))}function Z(e){z++||(W(),j&&e!=w&&(T[I]=e),r(M,c,[c,e]),r(_,c,[c,e]))}c=e.extend({},k,c);var O=c.success,M=c.error,_=c.complete,D=c.dataFilter,P=c.callbackParameter,H=c.callback,B=c.cache,j=c.pageCache,F=c.charset,I=c.url,q=c.data,R=c.timeout,U,z=0,W=t,X,V,J,K,Q,G;return S&&S(function(e){e.done(O).fail(M),O=e.resolve,M=e.reject}).promise(c),c.abort=function(){!(z++)&&W()},r(c.beforeSend,c,[c])===!1||z?c:(I=I||u,q=q?typeof q=="string"?q:e.param(q,c.traditional):u,I+=q?i(I)+q:u,P&&(I+=i(I)+encodeURIComponent(P)+"=?"),!B&&!j&&(I+=i(I)+"_"+(new Date).getTime()+"="),I=I.replace(/=\?(&|$)/,"="+H+"$1"),j&&(U=T[I])?U.s?Y(U.s[0]):Z(U):(E[H]=n,K=e(y)[0],K.id=l+N++,F&&(K[o]=F),L&&L.version()<11.6?(Q=e(y)[0]).text="document.getElementById('"+K.id+"')."+p+"()":K[s]=s,A&&(K.htmlFor=K.id,K.event=h),K[d]=K[p]=K[v]=function(e){if(!K[m]||!/i/.test(K[m])){try{K[h]&&K[h]()}catch(t){}e=C,C=0,e?Y(e[0]):Z(a)}},K.src=I,W=function(e){G&&clearTimeout(G),K[v]=K[d]=K[p]=null,x[g](K),Q&&x[g](Q)},x[f](K,J=x.firstChild),Q&&x[f](Q,J),G=R>0&&setTimeout(function(){Z(w)},R)),c)}var s="async",o="charset",u="",a="error",f="insertBefore",l="_jqjsp",c="on",h=c+"click",p=c+a,d=c+"load",v=c+"readystatechange",m="readyState",g="removeChild",y="<script>",b="success",w="timeout",E=window,S=e.Deferred,x=e("head")[0]||document.documentElement,T={},N=0,C,k={callback:l,url:location.href},L=E.opera,A=!!e("<div>").html("<!--[if IE]><i><![endif]-->").find("i").length;O.setup=function(t){e.extend(k,t)},e.jsonp=O})(jQuery)
@@ -53,6 +54,7 @@ var bodyTimeline = "";
                         bodyTimeline += '<div class="relleno relleno2"></div><div class="lineScroll"></div>';
                       }  
                       
+                      if(FinalValor.linknota=== ""){ var newlinkP = ""; }else{ var newlinkP = 'href="'+FinalValor.linknota+'"'; }
                         
                       if(FinalValor.typeevent === "worldcup" ){
                         bodyTimeline += '<article class="itemTimeline mundial" rel="'+newDate[2]+'">';  
@@ -62,23 +64,26 @@ var bodyTimeline = "";
 
                       
                       bodyTimeline += '<div class="yearItemTimeline">'+newDate[2]+'</div>';
-                      bodyTimeline += '<div class="infoItemTimeline">';
-                      if(FinalValor.typeevent === "worldcup"){
-                        bodyTimeline += '<div class="titleMundialItem">'+FinalValor.title+'</div>';
+                      bodyTimeline += '<div class="infoItemTimeline">';                      
+                      if(FinalValor.typeevent === "worldcup"){                                                
+                        bodyTimeline += '<a '+newlinkP+'><div class="titleMundialItem">'+FinalValor.title+'</div></a>';
                       }
-                      if(FinalValor.urlthumb != null){
-                        bodyTimeline += '<img src="'+FinalValor.urlthumb+'">';  
+                      if(FinalValor.urlthumb != null){                        
+                        bodyTimeline += '<a '+newlinkP+' ><img src="'+FinalValor.urlthumb+'"></a>';  
                       }
                       
-                      bodyTimeline += '<div class="summaryItemTimeline">'+jsonNew.period.description+'</div>';
+                      bodyTimeline += '<a '+newlinkP+'><div class="summaryItemTimeline">'+jsonNew.period.description+'</div></a>';
 
                       if(jsonNew.period.events){                       
                         if(jsonNew.period.events.event.length > 1 && FinalValor.typeevent != "worldcup"){
-                          $.each(jsonNew.period.events.event, function(index, val3) {                        
-                           bodyTimeline += '<div class="OtherTimeline">'+val3.description+'</div>';                        
+                          $.each(jsonNew.period.events.event, function(index, val3) {
+
+                            if(val3.link=== ""){ var newlinkDP = ""; }else{ var newlinkDP = 'href="'+val3.link+'"'; }
+
+                           bodyTimeline += '<a '+newlinkDP+'><div class="OtherTimeline">'+val3.description+'</div></a>';
                         });
                         }else{
-                          if(FinalValor.typeevent != "worldcup"){
+                          if(FinalValor.typeevent != "worldcup"){                            
                             bodyTimeline += '<div class="OtherTimeline">'+jsonNew.period.events.event.description+'</div>';
                           }
                         }
